f2afd97fe9a1556eca8e50719194d6d6a75a97eb
Joyce Huang <whuang152@my.bcit.ca>
2024-05-29 18:04:29 -0700

    fix the layout of task page and add icon for side bar

--- views/scripts/taskPage.js
+++ views/scripts/taskPage.js
@@ -134,10 +134,10 @@ async function showTodo(tasksData, tabType) {
                                                     <polyline points="1 5 4 8 11 1"></polyline>
                                                     </svg>
                                                 </span>
-                                                <span>${todo.title}</span>
+                                                    <span class="task-card-title">${todo.title}</span>
                                             </label>
                                             <button class="taskcard-delete-btn" id="taskcard-delete-btn">
-                                                <svg viewBox="0 0 15 17.5" height="13.5" width="11" xmlns="http://www.w3.org/2000/svg" class="icon">
+                                                <svg viewBox="0 0 15 17.5" height="15.5" width="13" xmlns="http://www.w3.org/2000/svg" class="icon">
                                                 <path transform="translate(-2.5 -1.25)" d="M15,18.75H5A1.251,1.251,0,0,1,3.75,17.5V5H2.5V3.75h15V5H16.25V17.5A1.251,1.251,0,0,1,15,18.75ZM5,5V17.5H15V5Zm7.5,10H11.25V7.5H12.5V15ZM8.75,15H7.5V7.5H8.75V15ZM12.5,2.5h-5V1.25h5V2.5Z" id="Fill"></path>
                                                 </svg>
                                             </button>
